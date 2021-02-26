89aee3568e8874b298675d15d25d234b0fa61b80
lucasparente1998 <72233781+lucasparente1998@users.noreply.github.com>
2021-02-26 18:25:01 -0300

    final da aula 5

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,11 +7,11 @@ function MyApp({ Component, pageProps }) {
 
 
   return (
-    <ChallengesProvider>
+    
       
         <Component {...pageProps} />
       
-    </ChallengesProvider>
+    
   )
 }
 
